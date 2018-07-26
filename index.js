931a99c45680b09b1405648fe33019fb168ad33e
wolfy1339 <webmaster@wolfy1339.com>
2018-07-26 02:06:23 -0400

    Fix remaining count (and use smaller then or equal to 4)

--- index.js
+++ index.js
@@ -146,7 +146,7 @@ module.exports = app => {
       let msg_base = `\x0313${payload.repository.name}\x0F/\x0306${ref}\x0F`;
 
       for (let c of payload.commits) {
-          if (count < 5) {
+          if (count <= 4) {
               c.message = c.message.split('\n')[0];
               let message = `${c.message.substring(0, 150)}${(c.message.length > 150 ? '...' : '')}`;
               let author = c.author.name || '(No author name)';
@@ -154,7 +154,7 @@ module.exports = app => {
               app.irc.privmsg(`${msg_base} \x0314${c.id.substring(0, 7)}\x0F ${author}: ${message}`);
               count++;
           } else {
-              app.irc.privmsg(`... and ${payload.commits.length - 5} more ${isM}.`);
+              app.irc.privmsg(`... and ${payload.commits.length - count} more ${isM}.`);
               break;
           }
       }
