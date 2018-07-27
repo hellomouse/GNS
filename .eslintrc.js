d50889f33fb22327441c2088c9d5ca8ba4ba50d8
wolfy1339 <webmaster@wolfy1339.com>
2018-07-26 23:48:24 -0400

    Remove jsx and browser env from eslint core config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,15 +2,11 @@ module.exports = {
     env: {
         es6: true,
         node: true,
-        browser: true,
         jest: true
     },
     parserOptions: {
         ecmaVersion: 2018,
         sourceType: 'module',
-        ecmaFeatures: {
-            jsx: true
-        }
     },
     extends: ['eslint:recommended', 'google'],
     rules: {
