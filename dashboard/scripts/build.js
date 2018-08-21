0adff3e1b2761e73cd1c67656409dcc3e96cbb99
wolfy1339 <webmaster@wolfy1339.com>
2018-08-21 18:25:15 -0400

    Use template strings in the react code

--- dashboard/scripts/build.js
+++ dashboard/scripts/build.js
@@ -56,16 +56,8 @@ measureFileSizesBeforeBuild(paths.appBuild)
       if (warnings.length) {
         console.log(chalk.yellow('Compiled with warnings.\n'));
         console.log(warnings.join('\n\n'));
-        console.log(
-          '\nSearch for the ' +
-            chalk.underline(chalk.yellow('keywords')) +
-            ' to learn more about each warning.'
-        );
-        console.log(
-          'To ignore, add ' +
-            chalk.cyan('// eslint-disable-next-line') +
-            ' to the line before.\n'
-        );
+        console.log(`\nSearch for the ${chalk.underline(chalk.yellow('keywords'))} to learn more about each warning.`);
+        console.log(`To ignore, add ${chalk.cyan('// eslint-disable-next-line')} to the line before.\n`);
       } else {
         console.log(chalk.green('Compiled successfully.\n'));
       }
