46449620e6f19f8daa40d5eab867a88d428c4234
BWBellairs <BWBellairs@outlook.com>
2018-08-21 22:04:03 +0100

    Removed un-needed comment
    Signed-off-by: BWBellairs <BWBellairs@outlook.com>

--- bot/labels.js
+++ bot/labels.js
@@ -3,7 +3,7 @@ module.exports = {
   invalid: '\x0301,08invalid\x0F',
   wontfix: '\x0301,00wontfix\x0F',
   duplicate: '\x0301,14duplicate\x0F',
-  enhancement: '\x0301,11enhancement\x0F', // \x301,11
+  enhancement: '\x0301,11enhancement\x0F',
   question: '\x0301,13question\x0F',
   'good first issue': '\x0301,06good first issue\x0F',
   'help wanted': '\x0301,03help wanted\x0F'
