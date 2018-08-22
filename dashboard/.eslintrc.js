327008f7dac67e6457678a1a7aff902e0f3dc1a5
wolfy1339 <webmaster@wolfy1339.com>
2018-08-22 16:42:46 -0400

    dashboard: Fix errors and some deprecations

--- dashboard/.eslintrc.js
+++ dashboard/.eslintrc.js
@@ -12,6 +12,19 @@ module.exports = {
             jsx: true
         }
     },
+    settings: {
+    react: {
+      createClass: 'createReactClass', // Regex for Component Factory to use,
+                                         // default to 'createReactClass'
+      pragma: 'React',  // Pragma to use, default to 'React'
+      version: '15.0', // React version, default to the latest React stable release
+      flowVersion: '0.53' // Flow version
+    },
+    propWrapperFunctions: ['forbidExtraProps']     // The names of any functions used to wrap the
+                                                   // propTypes object, e.g. `forbidExtraProps`.
+                                                   // If this isn't set, any propTypes wrapped in
+                                                   // a function will be skipped.
+    },
     plugins: ['react'],
     extends: ['plugin:react/recommended'],
     rules: {
