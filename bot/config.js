b6d6979c1376ae62fefccb58ec41594e24c19079
wolfy1339 <webmaster@wolfy1339.com>
2018-09-04 13:06:56 -0400

    Push config

--- bot/config.js
+++ bot/config.js
@@ -1,20 +1,24 @@
 module.exports = {
-  irc: {
-    server: 'irc.freenode.net',
-    port: 6697,
-    nickname: 'GNS',
-    ident: 'GNS',
-    realname: 'GitHub Notification Service',
-    NickServPass: '',
-    channel: '#YourGitChannel',
-    sasl: {
-      cert: null, // Path to cert for NickServ account for CertFP and sasl
-      key: null, // Path to private key that goes with certificate
-      key_passphrase: null
-    },
-    bindhost: null,
-    requireAuth: true
-  },
-  attentionString: '!att-{name}-{event}', // '!att-{org}-{name}-{event}'
-  multipleCommitsMaxLen: 4
+  orgs: {
+    hellomouse: {
+      irc: {
+        server: 'irc.freenode.net',
+        port: 6697,
+        nickname: 'GNS',
+        ident: 'GNS',
+        realname: 'GitHub Notification Service',
+        NickServPass: '',
+        channel: '#YourGitChannel',
+        sasl: {
+          cert: null, // Path to cert for NickServ account for CertFP and sasl
+          key: null, // Path to private key that goes with certificate
+          key_passphrase: null
+        },
+        bindhost: null,
+        requireAuth: true
+      },
+      attentionString: '!att-{name}-{event}', // '!att-{org}-{name}-{event}'
+      multipleCommitsMaxLen: 4
+    }
+  }
 };
