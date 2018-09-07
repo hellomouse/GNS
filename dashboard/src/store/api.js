03999113de16b9fca292842d659db5e520854f21
Nickolay Ilyushin <nickolay02@inbox.ru>
2018-09-07 22:09:52 +0300

    Added dummy apiGetRepoSettings function

--- dashboard/src/store/api.js
+++ dashboard/src/store/api.js
@@ -6,3 +6,25 @@ export const apiGetRepos = () => [
   'hellomouse/wtk',
   'handicraftsman/heliumbot'
 ];
+
+/** Returns settings for the given repository
+ * @param {String} repo
+ * @return {Object}
+ */
+export const apiGetRepoSettings = repo => {
+  return {
+    enabled: true,
+    ircHost: 'irc.freenode.net',
+    ircPort: '6667',
+    ircNick: 'GNS',
+    ircUser: 'TotallyNotGNS',
+    ircPass: 'APassword',
+    ircRnam: 'Github Notification System by HelloMouse',
+    ircChannel: '##helloworlders',
+    events: { // event names could be sent by the api server so you don't need to update dashboard code to add them
+      eventA: true,
+      eventB: false,
+      eventC: true
+    }
+  };
+};
