19ba6bff562d83c83f61229b6116b1b5dc296ec5
wolfy1339 <webmaster@wolfy1339.com>
2018-09-21 20:00:19 -0400

    Fix config for deps

--- dashboard/config/webpackDevServer.config.js
+++ dashboard/config/webpackDevServer.config.js
@@ -1,13 +1,13 @@
-const errorOverlayMiddleware = require('react-dev-utils/errorOverlayMiddleware');
-const noopServiceWorkerMiddleware = require('react-dev-utils/noopServiceWorkerMiddleware');
-const ignoredFiles = require('react-dev-utils/ignoredFiles');
+const errorOverlayMiddleware = require('@hellomouse/react-dev-utils/errorOverlayMiddleware');
+const noopServiceWorkerMiddleware = require('@hellomouse/react-dev-utils/noopServiceWorkerMiddleware');
+const ignoredFiles = require('@hellomouse/react-dev-utils/ignoredFiles');
 const config = require('./webpack.config.dev');
 const paths = require('./paths');
 
 const protocol = process.env.HTTPS === 'true' ? 'https' : 'http';
 const host = process.env.HOST || '0.0.0.0';
 
-module.exports = function(proxy, allowedHost) {
+module.exports = function (proxy, allowedHost) {
   return {
     // WebpackDevServer 2.4.3 introduced a security fix that prevents remote
     // websites from potentially accessing local content through DNS rebinding:
