1cca806422e127d39de87b08dd6dabbfe55f1421
wolfy1339 <webmaster@wolfy1339.com>
2018-09-23 00:52:21 -0400

    Add some boilerplate code to update DB
    It fetches the DB from upstream and then tries to get the document with the org name, then proceeds to add the repos from all the users orgs and sets them to the default values within the proper document

--- bot/web.js
+++ bot/web.js
@@ -79,8 +79,8 @@ module.exports = async function web(app) {
       secret: process.env.CLIENT_SECRET
     });
     octokit.authenticate({ type: 'oauth', token: req.session.access_token });
-    const emails = await octokit.users.getEmails();
-    const orgs = await octokit.users.getOrgs();
+    const emails = (await octokit.users.getEmails()).data;
+    const orgs = (await octokit.users.getOrgs()).data;
 
     const db = new PouchDB('http://91.92.144.105:5984/GNS');
     const defaults = {
@@ -94,14 +94,20 @@ module.exports = async function web(app) {
         orgDB = await db.get(org.login);
       } catch (e) {
         if (e.name === 'not_found') {
-          db.put({ ...defaults, _id: org.login })
+          db.put({ _id: org.login });
           orgDB = await db.get(org.login);
+          const repos = (await octokit.repos.getForOrgs({ org })).data;
+
+          for (let repo of repos) {
+            orgDB[repo.fullname] = defaults;
+          }
+          db.put(orgDB);
         }
       }
     }
     res.send(
-        `<p>You're logged in! Here's all your emails on GitHub: </p>${JSON.stringify(emails.data)}
-            <p>Here are all your orgs that I have access to: ${JSON.stringify(orgs.data)}
+        `<p>You're logged in! Here's all your emails on GitHub: </p>${JSON.stringify(emails)}
+            <p>Here are all your orgs that I have access to: ${JSON.stringify(orgs)}
             <p>Go back to <a href="./">log in page</a>.</p>`
     );
   });
