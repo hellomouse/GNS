3920eade0b85ddc7971c2350847b5a47422d0194
wolfy1339 <4595477+wolfy1339@users.noreply.github.com>
2018-10-09 13:28:32 -0400

    Fix push event (#67)
    * payload.base_ref is potentially undefined, so guard it in an if statement
    * Fix another error, isM isn't defined in the right spot
    * Fix another error, app.irc.privmsg is not a function
    We need to get the proper socket from the object
    * Fix some errors pertaining to the config

--- bot/index.js
+++ bot/index.js
@@ -280,12 +280,16 @@ module.exports = async app => {
       org = payload.repository.owner.login,
       distinct_commits = payload.commits.filter(x => x.distinct),
       [, ref_type, ref_name] = payload.ref.split('/'),
-      [,, base_ref_name] = payload.base_ref.split('/'),
+      base_ref_name = '',
       msg = [`${att}\x0F | \x0315${user}\x0F`],
       pushType = payload.forced ? 'force-pushed' : 'pushed',
       count = 1,
       repo = `${fmt_repo(payload.repository.name)}/${ref}`,
-      config = await db.get(org);
+      { config } = await db.get(org),
+      isM = (payload.commits.length || 1) === 1 ? 'commit' : 'commits', // Correct grammar for number of commits
+      url = await shortenUrl(payload.compare);
+
+    if (payload.base_ref) base_ref_name = payload.base_ref.split('/')[2];
 
     if (payload.created && config.detailedDeletesAndCreates) {
       if (ref_type === 'tag') {
@@ -317,14 +321,11 @@ module.exports = async app => {
       msg.push(`${pushType} \x02${numC}\x0F ${isM} to ${ref}`);
     }
 
-    let isM = (payload.commits.length || 1) === 1 ? 'commit' : 'commits', // Correct grammar for number of commits
-      url = await shortenUrl(payload.compare);
-
     msg.push(fmt_url(url));
-    app.irc.privmsg(msg.join(' '));
+    app.irc[org].privmsg(msg.join(' '));
 
     for (let c of payload.commits) {
-      if (count <= app.irc.config.multipleCommitsMaxLen) { // I know this isn't the best or prettiest solution, but it works
+      if (count <= config.multipleCommitsMaxLen) { // I know this isn't the best or prettiest solution, but it works
         c.message = c.message.split('\n')[0];
         let message = `${c.message.substring(0, 150)}${(c.message.length > 150 ? '...' : '')}`,
           author = fmt_name(c.author.name) || '\x02\x0304(No author name)\x0F';
