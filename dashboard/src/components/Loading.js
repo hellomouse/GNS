88a3ef1afd025f48716863b652c65e815ef675d8
wolfy1339 <webmaster@wolfy1339.com>
2018-10-09 23:52:13 -0400

    Center the Loading component

--- dashboard/src/components/Loading.js
+++ dashboard/src/components/Loading.js
@@ -1,17 +1,41 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import CircularProgress from '@material-ui/core/CircularProgress';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = {
+  flex: {
+    width: '100%',
+    display: 'flex',
+    'align-items': 'center',
+    'justify-content': 'center'
+  },
+  'flex-center': {
+    'align-self': 'flex-start'
+  }
+};
 
 /**
- * @return {React.ReactElementp[]}
+ * @param {*} props
+ * @return {React.ReactElement}
  */
-function Loading() {
+function Loading(props) {
   return (
-    <React.Fragment>
-      <CircularProgress color="secondary" />
-      <div>Loading...</div>
-    </React.Fragment>
+    <div className={props.classes.flex}>
+      <div className={props.classes['flex-center']}>
+        <CircularProgress color="secondary" size={100} />
+        <Typography variant="body1" gutterBottom>
+          Loading
+        </Typography>
+      </div>
+    </div>
   );
 }
 
-export default Loading;
+Loading.propTypes = {
+  classes: PropTypes.object
+};
+
+export default withStyles(styles)(Loading);
