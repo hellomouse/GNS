108cc7406b9cac4a9ad4090bc4685550bbaf5e74
wolfy1339 <webmaster@wolfy1339.com>
2018-10-10 22:52:10 -0400

    Add default values
    Should possibly change once we get repo settings

--- dashboard/src/components/IRCSettings.js
+++ dashboard/src/components/IRCSettings.js
@@ -15,16 +15,20 @@ export default class IRCSettings extends React.Component {
     return (
       <React.Fragment>
         <div className="sameline">
-          <TextField className="rsp-entry input-margin extra-width" id="ircHost" label="IRC server host" />
-          <TextField className="rsp-entry input-margin" id="ircPort" label="IRC server port" type="number" />
+          <TextField className="rsp-entry input-margin extra-width" id="ircHost" label="IRC server host"
+            defaultValue='chat.freenode.net' />
+          <TextField className="rsp-entry input-margin" id="ircPort" label="IRC server port" type="number"
+            defaultValue={6697} />
         </div>
         <div className="sameline">
-          <TextField className="rsp-entry input-margin" id="ircNick" label="IRC nickname" />
+          <TextField className="rsp-entry input-margin" id="ircNick" label="IRC nickname"
+            defaltValue='GNS' value='GNS' />
           <TextField className="rsp-entry input-margin" id="ircUser" label="IRC username/ident" />
         </div>
         <div className="sameline">
           <TextField className="rsp-entry input-margin" id="ircPass" label="IRC password (leave blank for none)" />
-          <TextField className="rsp-entry input-margin" id="ircRnam" label="IRC realname/gecos" />
+          <TextField className="rsp-entry input-margin" id="ircRnam" label="IRC realname/gecos"
+            defaultValue='GitHub Notification System by Hellomouse' />
         </div>
         <div className="sameline">
           <TextField className="rsp-entry input-margin" id="ircChannel" label="IRC channel" />
