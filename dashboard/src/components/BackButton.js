af51eeb4e9ef78b4443dd02458a393f310158c84
wolfy1339 <webmaster@wolfy1339.com>
2018-10-14 02:42:03 -0400

    Make back button actually useful

--- dashboard/src/components/BackButton.js
+++ dashboard/src/components/BackButton.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 import Button from '@material-ui/core/Button';
 import ArrowBack from '@material-ui/icons/ArrowBack';
 
 /** Custom Back button component */
 class BackButton extends React.Component {
-  onClick = () => {};
+  static propTypes = {
+    history: PropTypes.object.isRequired
+  };
+
+  onClick = () => { this.props.history.goBack(); };
   /**
    * @return {React.ReactElement}
    */
@@ -17,4 +26,4 @@ class BackButton extends React.Component {
   }
 }
 
-export default BackButton;
+export default withRouter(connect(null, null)(BackButton));
