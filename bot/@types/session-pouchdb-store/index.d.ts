ef946c4e47c20f79303f9a11bfbbb512fbf23b7f
wolfy1339 <webmaster@wolfy1339.com>
2018-11-13 09:22:52 -0500

    Add typings for some dependencies

---
+++ bot/@types/session-pouchdb-store/index.d.ts
@@ -0,0 +1,28 @@
+declare module "session-pouchdb-store" {
+  import session from "express-session";
+  import express from "express";
+
+  interface PouchSessionOptions extends session.SessionOptions {
+    maxIdle?: number
+    scavenge?: number
+    purge?: number
+  }
+
+
+  class SessionStore extends session.Store {
+    constructor(pouchdb?: PouchDB.Database | string, options?: PouchSessionOptions,)
+    regenerate: (req: express.Request, fn: (err?: any) => any) => void;
+    load: (sid: string, fn: (err: any, session?: Express.SessionData | null) => any) => void;
+    createSession: (req: express.Request, sess: Express.SessionData) => void;
+
+    get: (sid: string, callback: (err: any, session?: Express.SessionData | null) => void) => void;
+    set: (sid: string, session: Express.SessionData, callback?: (err?: any) => void) => void;
+    destroy: (sid: string, callback?: (err?: any) => void) => void;
+    all: (callback: (err: any, obj?: { [sid: string]: Express.SessionData; } | null) => void) => void;
+    length: (callback: (err: any, length?: number | null) => void) => void;
+    clear: (callback?: (err?: any) => void) => void;
+    touch: (sid: string, session: Express.SessionData, callback?: (err?: any) => void) => void;
+  }
+
+  export = SessionStore;
+}
